b98c99a794a98faa8edf9d021f91508560290071
Yaokang Wu <61270188+Hallimede@users.noreply.github.com>
2023-04-23 11:01:00 -0700

    modify random search

--- TestInput.py
+++ TestInput.py
@@ -1,10 +1,22 @@
-import unittest
 from Calculator import *
+import coverage
+from Prime import *
 # class TestCar(unittest.TestCase):
 def test():
+
+    cov = coverage.Coverage(branch=True)
+    cov.start()
+
+    # is_prime(4)
     quadratic(-70,-640,670,160,-704)
     quadratic(-56, 182,861,800,576)
     quadratic(-296,-257,-894,-879,-238)
-    # self.assertEqual(is_prime(5), False)
+
+    cov.stop()
+    data = cov.get_data()
+
+    print(data.measured_files())
+    print(data.arcs('/Users/hallimede/Downloads/DataScience-T4/Calculator.py'))
+
 
 test()
