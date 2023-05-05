d773875e611f080c95f53350757b1ded27b58582
Ziyu Huang <106014099+ziyu-Huang1@users.noreply.github.com>
2023-05-05 00:27:15 -0700

    add comment

--- nsga/MySampling.py
+++ nsga/MySampling.py
@@ -4,6 +4,11 @@ from pymoo.core.sampling import Sampling
 
 class MySampling(Sampling):
 
+    # initiate population:
+    # n_samples: size of population
+    # n_cases: number of test inputs in each individual
+    # generate test inouts according to the boundary of the test
+
     def _do(self, problem, n_samples, **kwargs):
         X = []
         for _ in range(n_samples):
